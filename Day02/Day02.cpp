5e68cacdce9309113fa7520d4505cdbff192a36f
Daniel Kisa <daniel.kisa@thermofisher.com>
2023-12-02 11:14:10 +0100

    day2 part1

---
+++ Day02/Day02.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Game {
+	int id;
+	vector<vector<int>> sets;
+	bool possible;
+};
+
+vector<string> split(const string& text, const string& separator) {
+	vector<string> splitText;
+	int startPos = 0;
+	int nextPos = text.find(separator, startPos);
+	while (nextPos >= 0) {
+		if (startPos == nextPos) {
+			startPos = nextPos + 1;
+			nextPos = text.find(separator, startPos);
+		}
+		else {
+			splitText.push_back(text.substr(startPos, nextPos - startPos));
+			startPos = nextPos + 1;
+			nextPos = text.find(separator, startPos);
+		}
+	}
+	splitText.push_back(text.substr(startPos));
+	return splitText;
+}
+
+Game parseLine(const string& line) {
+	vector<string> colors = { "red", "green", "blue" };
+	vector<int> maxCubes = { 12, 13, 14 };
+
+	vector<string> setStrings = split(line, ";");
+	string firstString = setStrings.at(0);
+	int id = stoi(firstString.substr(5, firstString.find(':') - 5));
+
+	bool possible = true;
+	vector<vector<int>> sets;
+	for (string setString : setStrings) {
+		vector<int> set;
+		for (int i = 0; i < colors.size(); i++) {
+			string color = colors.at(i);
+			int pos = setString.find(color);
+			if (pos > 1) {
+				vector<string> spaceSplit = split(setString.substr(0, pos - 1), " ");
+				int nCubes = stoi(spaceSplit.back());
+				if (nCubes > maxCubes.at(i)) {
+					possible = false;
+				}
+				set.push_back(nCubes);
+			}
+			else {
+				set.push_back(0);
+			}
+		}
+		sets.push_back(set);
+	}
+
+	return Game{ id, sets, possible };
+}
+
+int sumUpPossibleGames(vector<Game> games) {
+	int total = 0;
+	for (Game game : games) {
+		if (game.possible) {
+			total += game.id;
+		}
+	}
+	return total;
+}
+
+
+int main()
+{
+	// string testLine = "Game 1: 18 red, 8 green, 7 blue; 15 red, 4 blue, 1 green; 18 red, 1 green, 14 blue; 8 blue";
+
+	string inputPath = "input.txt";
+	fstream inputFile;
+	vector<string> inputData;
+
+	inputFile.open(inputPath, ios::in);
+	if (inputFile.is_open()) {
+		string line;
+		while (getline(inputFile, line)) {
+			inputData.push_back(line);
+		}
+		inputFile.close();
+	}
+
+	vector<Game> games;
+	for (string line : inputData) {
+		games.push_back(parseLine(line));
+	}
+
+	int totalPossible = sumUpPossibleGames(games);
+
+	cout << totalPossible;
+}
